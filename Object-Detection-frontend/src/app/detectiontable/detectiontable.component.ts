d733e54ae0937b64aaf734b2d193983764c91d9b
haiiamnikhil <haiiamnikhil@gmail.com>
2021-03-29 17:16:02 +0530

    Changing Template of History

--- Object-Detection-frontend/src/app/detectiontable/detectiontable.component.ts
+++ Object-Detection-frontend/src/app/detectiontable/detectiontable.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { ApiService } from '../services/api.service';
 
 @Component({
@@ -7,26 +7,39 @@ import { ApiService } from '../services/api.service';
   styleUrls: ['./detectiontable.component.css']
 })
 export class DetectiontableComponent implements OnInit {
-
+  
   singleDetectionHistory:any = []
   multiDetectionHistory:any = []
   singleDetectionfilesLength:number
   multiDetectionfilesLength:number
   singleDetectionPage: number = 1
   multiDetectionPage: number = 1
-  perPageItem:number = 5
-  constructor(private apiService: ApiService) { }
+  perPageItem:number = 10
+  viewMode:string
+  isDisplay:boolean = false
+
+  constructor(private apiService: ApiService, private reference: ElementRef) { }
 
   ngOnInit(){
+    this.apiService.currentmessage.subscribe(mode => this.viewMode = mode)
     this.apiService.detectionHistory().subscribe(response =>{
       if (response.status){
         this.singleDetectionHistory.push(response.singleDetection)
         this.multiDetectionHistory.push(response.multiDetection)
         this.singleDetectionfilesLength = this.singleDetectionHistory[0].length
         this.multiDetectionfilesLength = this.multiDetectionHistory[0].length
-        console.log(this.singleDetectionHistory)
       }
     },err => console.log(err))
+  }
+
+  showChild(event:any){
+    let getElements = event.currentTarget.parentNode.parentNode.nextSibling
+    if (getElements.style.display == 'block'){
+      getElements.style.display = 'none'
+    }
+    else{
+      getElements.style.display = 'block'
+    }
   }
 
 }
