e3fcec7ad7453af7c69841b746bbf346df4d7a4f
haiiamnikhil <haiiamnikhil@gmail.com>
2021-04-09 18:45:03 +0530

    Added State Management on Payment

--- Object-Detection-frontend/src/app/state/payment.actions.ts
+++ Object-Detection-frontend/src/app/state/payment.actions.ts
@@ -1,3 +1,5 @@
 import { createAction } from '@ngrx/store'
 
-export const plans = createAction('plans')
+export const basic = createAction('Basic')
+export const premium = createAction('premium')
+export const professional = createAction('professional')
--- Object-Detection-frontend/src/app/state/payment.reducer.ts
+++ Object-Detection-frontend/src/app/state/payment.reducer.ts
@@ -1,15 +1,14 @@
-import { plans } from './payment.actions';
+import { basic, premium, professional } from './payment.actions';
 import { on } from '@ngrx/store';
 import { selectedPackage } from './payment.state';
 import { createReducer } from '@ngrx/store';
 
 
-
-const _packageReducer = createReducer(selectedPackage, on(plans, (state) => {
-    return{
-        package: state.package = 'Basic'
-    }
-}))
+const _packageReducer = createReducer(selectedPackage,
+    on(basic, (state) => {return{package: state.package = 'Basic'}}),
+    on(premium,(state) => {return{package: state.package = 'Premium'}}),
+    on(professional, (state) => {return{package: state.package = 'Professional'}})
+    )
 
 export function packageReducer(state, action){
     return _packageReducer(state,action);
